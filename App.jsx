3c7b0c01519b921f222f1b8ac23720e1214b24ce
Rodrigo Sant Ana <128659017+rodr1g09@users.noreply.github.com>
2023-11-30 18:13:03 -0300

    começando o jogo

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+      <div>
+<p>Aqui teremos o jogo!</p>
+    </div>
+    
+  )
+}
+
+export default App
